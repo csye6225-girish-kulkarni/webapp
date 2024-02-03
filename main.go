00bf0e244249c5fd9921bd3c42f4af8f4c7e96d4
girish332 <kulkarni.gi@northeastern.edu>
2024-02-03 15:59:54 -0500

    Fixed unit test action failing

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	router := routerPkg.InitializeRouter()
 	err := router.Run(":8080")
 	if err != nil {
-		log.Println("error starting server: %v", err)
+		log.Printf("error starting server: %v", err)
 		return
 	}
 }
